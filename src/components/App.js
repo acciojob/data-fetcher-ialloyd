876fd1842a85f569515a2ef5535ba58fe87de315
ialloyd <iamalloydfds@gmail.com>
2023-10-22 19:15:08 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import axios from 'axios'
 
 const App = () => {
+
+  const [obj, setData] = useState({})
+
+  useEffect(() => {
+
+    axios.get('https://dummyjson.com/products')
+      .then(data => setData({ ...data.data }))
+      .catch(err => console.log(err))
+
+  }, [])
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    (!Object.keys(obj).length && <p>Loading.....</p>) || (Object.keys(obj).length && <div>
+      <h1>Data Fetched from API</h1>
+      <pre>{JSON.stringify(obj, null, 2)}</pre>
+    </div>)
   )
 }
 
-export default App
+export default App;
